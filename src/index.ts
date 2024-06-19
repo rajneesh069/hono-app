9d6c12f1b51633872906b52e12b821b09ce10cd2
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-06-19 19:25:27 +0530

    add some basic routes

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", async (c) => {
+  return c.text("Working");
+});
 
-export default app
+app.post("/home", async (c) => {
+  const body = await c.req.json();
+  console.log(body);
+  console.log(c.req.header("Authorization"));
+  console.log(c.req.query("param"));
+  return c.json({ message: "Hi there" });
+});
+
+export default app;
